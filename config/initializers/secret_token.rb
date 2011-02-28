ceb4e0dc7836349f1523d32a93c0bcabcc3dede4
mark <renoki@marks-MacBook-Pro.local>
2011-02-27 18:35:36 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmltest::Application.config.secret_token = 'f5f521ab259db69d485add806b9034cf3e2074ebe1a6bcb243f640b24fb310e684875b95eba009e5481b7973b9e183acf29afb3bda5bc7f9028248e09dae0773'
